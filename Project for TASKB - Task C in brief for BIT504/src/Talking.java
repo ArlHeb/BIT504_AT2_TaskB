4a5b86eb06a6d245d0aee79ce72486900b18d3b2
ArlHeb <arlodactylpsn@gmail.com>
2022-05-09 17:36:10 +1200

    Sharing my 504 task to GitHub.

---
+++ Project for TASKB - Task C in brief for BIT504/src/Talking.java
@@ -0,0 +1,14 @@
+
+
+public class Talking {
+	 public static void main(String[] args) {
+		 
+		 System.out.println("My name is Arleia Hebberd.");
+		 System.out.println("This is for my BIT504 AS2.");
+		 System.out.println("Java programming is interesting."); // Comments about myself
+		 System.out.println("This is my final task in the project.");
+	       
+	}
+}
+	
+
